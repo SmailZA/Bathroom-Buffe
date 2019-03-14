547b0fd9386ac0c6351c7500ffc0500fb02dfc90
Kasper Enwall <karl.kasper.enwall@hotmail.com>
2019-03-14 14:53:03 +0100

    Pink bubs make ye slow (all bubs currently have pink behaviour)

--- Prototype/Bathroom_Buffé/Assets/Code/BubbleBehaviours/PinkBubble.cs
+++ Prototype/Bathroom_Buffé/Assets/Code/BubbleBehaviours/PinkBubble.cs
@@ -5,13 +5,31 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "BubbleBehaviour/Pink")]
 public class PinkBubble : BubbleBehaviour
 {
+    public float slowDownDuration;
+    public FloatVariable defaultFlySpeed;
+    public FloatVariable slowDownVariable;
+
     public override void OnShot(GameObject thisGO, GameObject shotByGO)
     {
+        // Drop power up
         Destroy(thisGO);
     }
     
-    public override void OnCollided(GameObject thisGO, GameObject shotByGO)
+    public override void OnCollided(GameObject thisGO, GameObject collidedWithGO)
     {
+        FlyController controller = collidedWithGO.GetComponent<FlyController>();
+        if (controller)
+        {
+            controller.flightSpeedVariable = slowDownVariable;
+            Debug.Log("Slowed speed to: " + controller.flightSpeedVariable.value);
+            collidedWithGO.GetComponent<FlyController>().StartCoroutine(ResetSpeed(controller)); 
+        }
+    }
 
+    IEnumerator ResetSpeed(FlyController controller)
+    {
+        yield return new WaitForSeconds(slowDownDuration);
+        controller.flightSpeedVariable = defaultFlySpeed;
+        Debug.Log("Reset speed to: " + controller.flightSpeedVariable.value);
     }
 }
