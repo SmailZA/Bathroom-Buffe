609f5a3b444129de8fbd6cc2a37465a35d226719
Driton84 <driton.gashi84@gmail.com>
2019-03-19 16:41:29 +0100

    Sennding in boubles

---
+++ Prototype/Bathroom_Buffé/Assets/Code/BubbleBehaviours/BackGroundBoubleBurst.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class BackGroundBoubleBurst : MonoBehaviour
+{
+    public int Timer = 1;
+    public int MaxTimer = 300;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Timer > MaxTimer)
+        {
+            GetComponent<Animator>().SetBool("PlayBubbleAnimation", true);
+            RandomTimer();
+        }
+        else
+        {
+            Timer++;
+            GetComponent<Animator>().SetBool("PlayBubbleAnimation", false);
+        }
+           
+
+    }
+
+    void RandomTimer ()
+    {
+        float tempScale = Random.Range(0.3f, 1.2f);
+        this.transform.localScale = new Vector3(tempScale, tempScale, tempScale);
+        Timer = Random.Range(1 , MaxTimer-100);
+    }
+}
